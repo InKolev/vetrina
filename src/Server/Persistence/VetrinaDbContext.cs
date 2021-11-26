0f5e5acbbc8fe7ed87a49f9905667d8d1961b346
Ivan Kolev <ivan.kolev@tribalgroup.com>
2021-11-26 03:42:47 +0200

    Added DbContext.

---
+++ src/Server/Persistence/VetrinaDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vetrina.Server.Domain;
+
+namespace Vetrina.Server.Persistence
+{
+    public class VetrinaDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public VetrinaDbContext(DbContextOptions<VetrinaDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
