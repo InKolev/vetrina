15b2e0362458bab79eeef5883fcf47447bf4ecdc
Ivan Kolev <ivan.kolev@tribalgroup.com>
2021-11-26 03:38:09 +0200

    Added options (configuration) classes.

---
+++ src/Server/Options/JwtOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Vetrina.Server.Options
+{
+    public class JwtOptions
+    {
+        /// <summary>
+        /// The secret used for signing JWTs.
+        /// </summary>
+        public string Secret { get; set; }
+
+        /// <summary>
+        /// Refresh token time to live (in days).
+        /// Inactive tokens are automatically deleted from the database after this time expires.
+        /// </summary>
+        public int RefreshTokenTimeToLive { get; set; }
+    }
+}
