2abcd5c59aadf166fa7dd260e43e5e66207b50ad
Ivan Kolev <ivan.kolev@tribalgroup.com>
2022-03-30 22:32:21 +0300

    Export promotions card as a separate component and update nuget package versions to latest

--- src/Server/Migrations/20220221230644_Initial.Designer.cs
+++ src/Server/Migrations/20220221230644_Initial.Designer.cs
@@ -19,7 +19,7 @@ namespace Vetrina.Server.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.1")
+                .HasAnnotation("ProductVersion", "6.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
